a1a8e396a7a7786527bb6f8b65127c43af8b015b
Guilherme Amaral <guilherme.amaral@avenuecode.com>
2023-12-05 10:04:22 -0300

    Add transaction use case

---
+++ internal/usecase/create_transaction/create_transaction_test.go
@@ -0,0 +1,64 @@
+package createtransaction
+
+import (
+	"testing"
+
+	"github.com/guiaamaral/fullcycle-ms-wallet/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type TransactionGatewayMock struct {
+	mock.Mock
+}
+
+func (m *TransactionGatewayMock) Create(client *entity.Transaction) error {
+	args := m.Called(client)
+	return args.Error(0)
+}
+
+type AccountGatewayMock struct {
+	mock.Mock
+}
+
+func (m *AccountGatewayMock) Save(account *entity.Account) error {
+	args := m.Called(account)
+	return args.Error(0)
+}
+
+func (m *AccountGatewayMock) FindById(id string) (*entity.Account, error) {
+	args := m.Called(id)
+	return args.Get(0).(*entity.Account), args.Error(1)
+}
+
+func TestCreateTransactionUseCaseExecute(t *testing.T) {
+	client1, _ := entity.NewClient("John Doe", "j@j.com")
+	account1 := entity.NewAccount(client1)
+	account1.Credit(1000)
+
+	client2, _ := entity.NewClient("John Doe 1", "j1@j.com")
+	account2 := entity.NewAccount(client2)
+	account2.Credit(1000)
+
+	mockAccount := &AccountGatewayMock{}
+	mockAccount.On("FindById", account1.ID).Return(account1, nil)
+	mockAccount.On("FindById", account2.ID).Return(account2, nil)
+
+	mockTransaction := &TransactionGatewayMock{}
+	mockTransaction.On("Create", mock.Anything).Return(nil)
+
+	inputDto := CreateTransactionInputDTO{
+		AccountIDFrom: account1.ID,
+		AccountIDTo:   account2.ID,
+		Amount:        500,
+	}
+
+	uc := NewCreateTransactionUseCase(mockAccount, mockTransaction)
+	output, err := uc.Execute(inputDto)
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+	mockAccount.AssertExpectations(t)
+	mockTransaction.AssertExpectations(t)
+	mockAccount.AssertNumberOfCalls(t, "FindById", 2)
+	mockTransaction.AssertNumberOfCalls(t, "Create", 1)
+}
