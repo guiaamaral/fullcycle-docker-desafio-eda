d00198f47fdc8384d0cc47e900f62d4acb762fef
Guilherme Amaral <guilherme.amaral@avenuecode.com>
2023-12-04 22:02:43 -0300

    Add create account use case

---
+++ internal/gateway/account.go
@@ -0,0 +1,8 @@
+package gateway
+
+import "github.com/guiaamaral/fullcycle-ms-wallet/internal/entity"
+
+type AccountGateway interface {
+	Save(client *entity.Account) error
+	FindById(id string) (*entity.Account, error)
+}
---
+++ internal/usecase/create_account/create_account.go
@@ -0,0 +1,41 @@
+package createaccount
+
+import (
+	"github.com/guiaamaral/fullcycle-ms-wallet/internal/entity"
+	"github.com/guiaamaral/fullcycle-ms-wallet/internal/gateway"
+)
+
+type CreateAccountInputDto struct {
+	ClientID string
+}
+
+type CreateAccountOutputDto struct {
+	ID string
+}
+
+type CreateAccountUseCase struct {
+	AccountGateway gateway.AccountGateway
+	ClientGateway  gateway.ClientGateway
+}
+
+func NewCreateAccountUseCase(accountGateway gateway.AccountGateway, clientGateway gateway.ClientGateway) *CreateAccountUseCase {
+	return &CreateAccountUseCase{
+		AccountGateway: accountGateway,
+		ClientGateway:  clientGateway,
+	}
+}
+
+func (uc *CreateAccountUseCase) Execute(input CreateAccountInputDto) (*CreateAccountOutputDto, error) {
+	client, err := uc.ClientGateway.Get(input.ClientID)
+	if err != nil {
+		return nil, err
+	}
+	account := entity.NewAccount(client)
+	err = uc.AccountGateway.Save(account)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateAccountOutputDto{
+		ID: account.ID,
+	}, nil
+}
