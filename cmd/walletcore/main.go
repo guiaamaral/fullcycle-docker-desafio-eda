d01860753673b58a16cb7bf59e7486663dbb2b0e
Guilherme Amaral <guilherme.amaral@external.stellantis.com>
2023-12-08 14:52:58 -0300

    Some adjusts

--- cmd/walletcore/main.go
+++ cmd/walletcore/main.go
@@ -7,27 +7,36 @@ import (
 
 	"github.com/guiaamaral/fullcycle-ms-wallet/internal/database"
 	"github.com/guiaamaral/fullcycle-ms-wallet/internal/event"
+	"github.com/guiaamaral/fullcycle-ms-wallet/internal/event/handler"
 	"github.com/guiaamaral/fullcycle-ms-wallet/internal/usecase/create_account"
 	"github.com/guiaamaral/fullcycle-ms-wallet/internal/usecase/create_client"
 	"github.com/guiaamaral/fullcycle-ms-wallet/internal/usecase/create_transaction"
 	"github.com/guiaamaral/fullcycle-ms-wallet/internal/web"
 	"github.com/guiaamaral/fullcycle-ms-wallet/internal/web/webserver"
 	"github.com/guiaamaral/fullcycle-ms-wallet/pkg/events"
+	"github.com/guiaamaral/fullcycle-ms-wallet/pkg/kafka"
 	"github.com/guiaamaral/fullcycle-ms-wallet/pkg/uow"
 
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "localhost", "3306", "wallet"))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "mysql", "3306", "wallet"))
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
+	configMap := ckafka.ConfigMap{
+		"bootstrap.servers": "kafka:29092",
+		"group.id":          "wallet",
+	}
+	kafkaProducer := kafka.NewKafkaProducer(&configMap)
+
 	eventDispatcher := events.NewEventDispatcher()
+	eventDispatcher.Register("TransactionCreated", handler.NewTransactionCreatedKafkaHandler(kafkaProducer))
 	TransactionCreatedEvent := event.NewTransactionCreated()
-	// eventDispatcher.Register("TransactionCreated", handler)
 
 	clientDb := database.NewClientDB(db)
 	accountDb := database.NewAccountDB(db)
@@ -45,7 +54,7 @@ func main() {
 	createAccountUseCase := create_account.NewCreateAccountUseCase(accountDb, clientDb)
 	createTransactionUseCase := create_transaction.NewCreateTransactionUseCase(uow, eventDispatcher, TransactionCreatedEvent)
 
-	webserver := webserver.NewWebServer(":3000")
+	webserver := webserver.NewWebServer(":8080")
 
 	clientHandler := web.NewWebClientHandler(*createClientUseCase)
 	accountHandler := web.NewWebAccountHandler(*createAccountUseCase)
@@ -55,5 +64,6 @@ func main() {
 	webserver.AddHandler("/accounts", accountHandler.CreateAccount)
 	webserver.AddHandler("/transactions", transactionHandler.CreateTransaction)
 
+	fmt.Println("Server is runing")
 	webserver.Start()
 }
