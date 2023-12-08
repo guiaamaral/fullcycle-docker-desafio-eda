eace43e253bf319a253d3562ca86b5df2a710d8e
Guilherme Amaral <guilherme.amaral@external.stellantis.com>
2023-12-08 17:01:16 -0300

    balance: Add kafka

--- balance-service/src/main.ts
+++ balance-service/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        // brokers: ['kafka:29092'],
+        brokers: ['localhost:9092'],
+      },
+    },
+  });
+
+  await app.listen(3003);
 }
+
 bootstrap();
